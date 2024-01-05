ccb48a1a8315b90df911a594616c1fdb1b42c495
ANGHOOO <leeanghoo2@gmail.com>
2024-01-05 11:43:56 +0900

    상하좌우 - Solved
    기본적인 구현 문제의 유형.
    2차원 격자 상에서의 이동을 편하게 하기 위해 dxs, dys 배열을 생성한 뒤 풀이 진행.
    격자 상의 범위를 벗어났을 때는 이동하지 않게 예외 케이스를 작성하는 것이 중요.

--- Book/Implementation/udlr.py
+++ Book/Implementation/udlr.py
@@ -1,1 +1,24 @@
-write later
+N = 5
+direction = ['R', 'R', 'R', 'U', 'D', 'D']
+start_x, start_y = 1, 1
+
+dxs, dys = [0, 0, -1, 1], [-1, 1, 0, 0]
+
+mapper = {
+    'L' : 0,
+    'R' : 1,
+    'U' : 2,
+    'D' : 3
+}
+
+for direc in direction:
+    d = mapper[direc]
+    dx, dy = dxs[d], dys[d]
+    new_x, new_y = start_x + dx, start_y + dy
+
+    if new_x < 1 or new_x > N or new_y < 1 or new_y > N:
+        continue
+
+    start_x, start_y = new_x, new_y
+
+print(start_x, start_y)
