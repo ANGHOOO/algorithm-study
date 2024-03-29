ec04d1465a4a1f85d13ef752597bcd4201a44712
ANGHOOO <leeanghoo2@gmail.com>
2024-03-29 11:36:07 +0900

    solve again about this problem

--- Book/Greedy/number_card_game.py
+++ Book/Greedy/number_card_game.py
@@ -1,25 +1,15 @@
-# 나의 답안 --> 2중 반복문을 사용하여 최소값을 배열에 저장한 뒤 마지막에 해당 배열에서 최대값 찾기
-n, m = 2, 4
-arr = [[7, 3, 1, 8], [3, 3, 3, 4]]
-
-answer = []
-
-for i in range(n):
-    min_num = float('inf')
-    for j in range(m):
-        min_num = min(min_num, arr[i][j])
-    answer.append(min_num)
-
-print(max(answer))
-    
-# 책의 답안 --> 하나의 반복문을 사용하여 최소값과 최대값을 같이 업데이트
 n, m = map(int, input().split())
+arr = []
 
-result = 0
+for _ in range(n):
+    temp = list(map(int, input().split()))
+    arr.append(temp)
+
+max_num = 0
+
 for i in range(n):
-    data = list(map(int, input().split()))
-    min_value = min(data)
-    result = max(result, min_value)
+    temp = min(arr[i])
+    if temp > max_num:
+        max_num = temp 
 
-print(result)
-    
+print(max_num)
