2622d766a118e10601083a3b30db5849fbd6848c
ANGHOOO <leeanghoo2@gmail.com>
2024-04-02 10:32:39 +0900

    solve again about this problem

--- Book/Implementation/three_in_time.py
+++ Book/Implementation/three_in_time.py
@@ -1,13 +1,29 @@
-n = 5
+# my solution
+n = int(input())
 
-minute = 59
-second = 59
-count = 0
+answer = 0
 
-for i in range(n+1):
-    for j in range(minute+1):
-        for k in range(second+1):
-            if '3' in str(i) or '3' in str(j) or '3' in str(k):
-                count += 1
+total_time = n * 60 * 60 + 59 * 60 + 59
 
-print(count)
+for t in range(total_time+1):
+    hour = t // (60 * 60)
+    t = t % (60 * 60)
+    minute = t // 60
+    t = t % 60
+    second = t % 60
+    if '3' in str(hour) or '3' in str(minute) or '3' in str(second):
+        answer += 1
+
+print(answer)
+
+# book solution
+h = int(input())
+answer = 0
+
+for i in range(h+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                answer += 1
+
+print(answer)
