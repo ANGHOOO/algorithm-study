b868497d9de3c687515a2eab200b654424f4d394
ANGHOOO <leeanghoo2@gmail.com>
2024-04-03 10:35:06 +0900

    solve again about this problem

--- Book/Implementation/knight_of_kingdom.py
+++ Book/Implementation/knight_of_kingdom.py
@@ -1,77 +1,24 @@
-location = 'c2'
-arr = [[0] * 8 for _ in range(8)]
-loc = {
-    'a' : 1,
-    'b' : 2,
-    'c' : 3,
-    'd' : 4,
-    'e' : 5,
-    'f' : 6,
-    'g' : 7,
-    'h' : 8
-}
+point = list(input())
+x, y = point[0], point[1]
+x_mapper = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+x = x_mapper.index(x)
+y = int(y) - 1
+answer = 0
 
-def cant_go(x, y):
-    return x < 1 or y < 1 or x > 8 or y > 8
+def in_range(x, y):
+    if x < 0 or y < 0 or x > 7 or y > 7:
+        return False
+    else:
+        return True
 
-def moving(location):
-    location = list(location)
-    x, y = location[1], location[0]
-    y, x = loc[y], int(x)
-    count = 0
+# 수평으로 두 칸 이동 후 수직으로 한 칸
+# 수직으로 두 칸 이동 후 수평으로 한 칸
+dxs, dys = [-2, -2, 2, 2, -1, 1, -1, 1], [-1, 1, -1, 1, -2, -2, 2, 2]
 
-    # case1) 아래로 두 칸, 왼쪽으로 한 칸
-    nx, ny = x + 2, y - 1
-    if not cant_go(nx, ny):
-        count += 1
-    # case2) 아래로 두 칸, 오른쪽으로 한 칸
-    nx, ny = x + 2, y + 1
-    if not cant_go(nx, ny):
-        count += 1
-    # case3) 위로 두 칸, 왼쪽으로 한 칸
-    nx, ny = x - 2, y - 1
-    if not cant_go(nx, ny):
-        count += 1
-    # case4) 위로 두 칸, 오른쪽으로 한 칸
-    nx, ny = x - 2, y + 1
-    if not cant_go(nx, ny):
-        count += 1
-    # case5) 왼쪽으로 두 칸, 위로 한 칸
-    nx, ny = x - 1, y - 2
-    if not cant_go(nx, ny):
-        count += 1
-    # case6) 왼쪽으로 두 칸, 아래로 한 칸
-    nx, ny = x + 1, y - 2
-    if not cant_go(nx, ny):
-        count += 1
-    # case7) 오른쪽으로 두 칸, 위로 한 칸
-    nx, ny = y + 2, x - 1
-    if not cant_go(nx, ny):
-        count += 1
-    # case8) 오른쪽으로 두 칸, 아래로 한 칸
-    nx, ny = y + 2, x + 1
-    if not cant_go(nx, ny):
-        count += 1
+for dx, dy in zip(dxs, dys):
+    nx, ny = x + dx, y + dy 
+    if in_range(nx, ny):
+        answer += 1
 
-    return count
-
-print(moving(location))
-
-# 책 풀이
-input_data = input()
-row = int(input_data[1])
-column = int(ord(input_data[0])) - int(ord('a')) + 1
-
-# 나이트가 이동할 수 있는 8가지 방향 정의
-steps = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
-
-# 8가지 방향에 대하여 각 위치로 이동이 가능한지 확인
-result = 0
-for step in steps:
-    next_row = row + step[0]
-    next_column = column + step[1]
-    # 해당 위치로 이동이 가능하다면 카운트 증가
-    if next_row >= 1 and next_row <= 8 and next_column >= 1 and next_column <= 8:
-        result += 1
-
-print(result)
+print(answer)
+    
