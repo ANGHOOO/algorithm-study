d24ed2f31e34d52418315abbd12331fe9c04cceb
ANGHOOO <leeanghoo2@gmail.com>
2024-04-05 14:06:57 +0900

    study again about bfs and dfs

--- Book/DFS_BFS/basic_concept.py
+++ Book/DFS_BFS/basic_concept.py
@@ -1,7 +1,6 @@
 def dfs(graph, v, visited):
-    visited[v] = True
+    visited[v] = True 
     print(v, end=' ')
-    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
     for i in graph[v]:
         if not visited[i]:
             dfs(graph, i, visited)
@@ -19,6 +18,7 @@ graph = [
 ]
 
 visited = [False] * 9
+
 dfs(graph, 1, visited)
 
 print()
@@ -26,28 +26,29 @@ print()
 from collections import deque
 
 def bfs(graph, start, visited):
-    queue = deque([start])
-    visited[start] = True
+    queue = deque([start])    
+    visited[start] = True 
 
     while queue:
         v = queue.popleft()
-        print(v, end=' ')
+        print(v, end = ' ')
         for i in graph[v]:
             if not visited[i]:
                 queue.append(i)
-                visited[i] = True
+                visited[i] = True 
 
 graph = [
     [],
-    [2, 3, 8], # 1번 노드와 연결된 노드
-    [1, 7], # 2번 노드와 연결된 노드
-    [1, 4, 5], # 3번 노드와 연결된 노드
-    [3, 5], # 4번 노드와 연결된 노드
-    [3, 4], # 5번 노드와 연결된 노드
-    [7], # 6번 노드와 연결된 노드
-    [2, 6, 8], # 7번 노드와 연결된 노드
-    [1, 7] # 8번 노드와 연결된 노드
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
 ]
 
 visited = [False] * 9
+
 bfs(graph, 1, visited)
