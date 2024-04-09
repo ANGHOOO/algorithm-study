43da87f8b5f7b490688e8af5051779a38e3b1a79
ANGHOOO <leeanghoo2@gmail.com>
2024-04-09 18:05:32 +0900

    solve again about this problem

--- Book/DFS_BFS/escape_maze.py
+++ Book/DFS_BFS/escape_maze.py
@@ -1,34 +1,28 @@
-from collections import deque
+from collections import deque 
 
-n, m = 5, 6
-arr = [
-    [1, 0, 1, 0, 1, 0],
-    [1, 1, 1, 1, 1, 1],
-    [0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1]
-]
+n, m = map(int, input().split())
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
 
-visited = [[0] * m for _ in range(n)]
-count = 0
+dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
 
 def bfs(x, y):
-    global arr
     q = deque()
     q.append((x, y))
-    dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
 
     while q:
         x, y = q.popleft()
         for dx, dy in zip(dxs, dys):
-            nx, ny = x + dx, y + dy
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                continue
+            nx, ny = x + dx, y + dy 
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue 
             if arr[nx][ny] == 0:
-                continue
+                continue 
             if arr[nx][ny] == 1:
+                arr[nx][ny] = arr[x][y] + 1 
                 q.append((nx, ny))
-                arr[nx][ny] = arr[x][y] + 1
 
 bfs(0, 0)
+
 print(arr[n-1][m-1])
