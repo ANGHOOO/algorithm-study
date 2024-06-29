fdb656cc123bc4df1ec4fa0fa979f6caf8acacb6
ANGHOOO <leeanghoo2@gmail.com>
2024-06-29 22:33:04 +0900

    [이코테] 문자열 뒤집기

--- Book/Greedy/문자열 뒤집기.py
+++ Book/Greedy/문자열 뒤집기.py
@@ -1,10 +1,17 @@
-arr = list(input())
-start = arr[0]
-count = 0
+data = input()
+count0 = 0
+count1 = 0
 
-for i in range(1, len(arr)):
-    if arr[i] != start:
-        start = arr[i]
-        count += 1
+if data[0] == '1':
+    count0 += 1
+else:
+    count1 += 1
 
-print(count - 1)
+for i in range(len(data) - 1):
+    if data[i] != data[i+1]:
+        if data[i+1] == '1':
+            count0 += 1
+        else:
+            count1 += 1
+
+print(min(count0, count1))
