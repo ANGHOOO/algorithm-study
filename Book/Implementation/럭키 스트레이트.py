c3a92a97a63464c7be8caacef9d96be5113e4c08
ANGHOOO <leeanghoo2@gmail.com>
2024-07-01 16:01:07 +0900

    [이코테] 럭키  스트레이트

--- Book/Implementation/럭키 스트레이트.py
+++ Book/Implementation/럭키 스트레이트.py
@@ -1,7 +1,14 @@
 arr = list(map(int, input()))
 length = len(arr)
+criterion = length // 2
 
-if sum(arr[:length // 2]) == sum(arr[length // 2:]):
+left = arr[:criterion]
+right = arr[criterion:]
+
+left_sum = sum(left)
+right_sum = sum(right)
+
+if left_sum == right_sum:
     print('LUCKY')
 else:
     print('READY')
