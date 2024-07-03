660a0c751c39389f83231749cdaa74e08264bd46
ANGHOOO <leeanghoo2@gmail.com>
2024-07-03 15:34:27 +0900

    BFS basic concept

--- Book/DFS_BFS/basic_concept.py
+++ Book/DFS_BFS/basic_concept.py
@@ -26,3 +26,39 @@ graph = [
 
 visited = [False] * 9
 dfs(graph, 1, visited) # 1->2->7->6->8->3->4->5
+
+# BFS(Breadth First Search) : 너비 우선 탐색
+# 재귀 함수를 통해 구현하는 DFS에 비해 일반적으로 실행 시간이 더 빠름
+
+# BFS의 동작 방식
+# 1. 시작 노드를 큐에 삽입하고 방문 처리
+# 2. 큐에 삽입된 노드를 꺼내서 해당 노드와 인접 한 노드 중 방문하지 않은 노드 모두를 큐에 삽입하고 방문 처리
+# 3. 큐가 빌때까지 2번의 과정을 반복 수행
+from collections import deque 
+
+def bfs(graph, start, visited):
+    q = deque([start])
+    visited[start] = True 
+
+    while q:
+        v = q.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                q.append(i)
+                visited[i] = True 
+
+graph = [
+    [],
+    [2, 3, 8], # 1번 노드
+    [1, 7], # 2번 노드
+    [1, 4, 5], # 3번 노드
+    [3, 5], # 4번 노드
+    [3, 4], # 5번 노드
+    [7], # 6번 노드
+    [2, 6, 8], # 7번 노드
+    [1, 7] # 8번 노드
+]
+
+visited = [False] * 9
+bfs(graph, 1, visited) # 1 -> 2 -> 3 -> 8 -> 7 -> 4 -> 5 -> 6
