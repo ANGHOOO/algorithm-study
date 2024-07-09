617e37f1ad587be77727aa4c8424462d9f580930
ANGHOOO <leeanghoo2@gmail.com>
2024-07-09 16:01:55 +0900

    [이코테] 공유기 설치

---
+++ Book/Search/공유기 설치.py
@@ -0,0 +1,28 @@
+n, c = map(int, input().split())
+arr = []
+for _ in range(n):
+    arr.append(int(input()))
+
+arr.sort()
+
+start = 1
+end = arr[-1] - arr[0]
+result = 0
+
+while start <= end:
+    mid = (start + end) // 2 
+    value = arr[0]
+    count = 1
+
+    for i in range(1, n):
+        if arr[i] >= value + mid:
+            count += 1
+            value = arr[i]
+
+    if count >= c:
+        result = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+        
+print(result)
